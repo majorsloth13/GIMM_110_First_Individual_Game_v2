8f3b727e1606cd4d7e5eb31208a95afba63f48c1
majorsloth13 <samgottsch@gmail.com>
2025-10-28 00:12:31 -0600

    Level 2 created
    Level 2 and other assets created

--- GIMM_110_First_Individual_Game_v2/Assets/Scripts/Health_Death_Script/DeathBox.cs
+++ GIMM_110_First_Individual_Game_v2/Assets/Scripts/Health_Death_Script/DeathBox.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DeathBox : MonoBehaviour
 {
     [SerializeField] private float damageAmount = 999f; // Enough to kill the player
+    [SerializeField] private Color gizmoColor = new Color(1f, 0f, 0f, 0.3f); // Semi-transparent red
 
     protected virtual void OnTriggerEnter2D(Collider2D other)
     {
@@ -22,6 +23,28 @@ public class DeathBox : MonoBehaviour
                 playerHealth.TakeDamage(damageAmount);
                 
             }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Set color for visualization in the editor
+        Gizmos.color = gizmoColor;
+
+        // Try to match collider bounds (works for BoxCollider2D and CircleCollider2D)
+        Collider2D col = GetComponent<Collider2D>();
+        if (col is BoxCollider2D box)
+        {
+            // Draw a filled box that matches the trigger’s size and position
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawCube(box.offset, box.size);
+            Gizmos.color = Color.red; // Draw outline in red
+            Gizmos.DrawWireCube(box.offset, box.size);
+        }
+        else if (col is CircleCollider2D circle)
+        {
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawWireSphere(circle.offset, circle.radius);
         }
     }
 }
